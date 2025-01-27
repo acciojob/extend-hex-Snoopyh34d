7778b1ac6760270639d30d0f9a4c41fa0d1939b3
Prabhat Ayush <60286121+Snoopyh34d@users.noreply.github.com>
2025-01-27 19:17:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const extendHex = (shortHex) => {
   // write your code here
+	//shortHex ="#bA0"
+
+	let i = shortHex.length - 3, isLower = true;
+	let output = "#";
+
+	while (i<shortHex.length) {
+
+		let asciiCode = shortHex[i].charCodeAt(0);
+		if (asciiCode >=97 && asciiCode <=122) {
+			isLower = flase;
+		}
+		output += shortHex[i] + shortHex[i];
+		i++;
+	}
+	return isLower ? output.LowerCase() : output;
 };
 
 // Do not change the code below.
