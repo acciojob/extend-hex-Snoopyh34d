bd66f3ceb7234ffed8568cb821a5a67e60719168
Prabhat Ayush <60286121+Snoopyh34d@users.noreply.github.com>
2025-01-27 19:19:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ const extendHex = (shortHex) => {
   // write your code here
 	//shortHex ="#bA0"
 
-	let i = shortHex.length - 3, isLower = true;
+	let i = shortHex.length - 3, isCapital = false;
 	let output = "#";
 
 	while (i<shortHex.length) {
 
 		let asciiCode = shortHex[i].charCodeAt(0);
-		if (asciiCode >=97 && asciiCode <=122) {
-			isLower = flase;
+		if (asciiCode >=65 && asciiCode <=90) {
+			isCapital = true;
 		}
 		output += shortHex[i] + shortHex[i];
 		i++;
 	}
-	return isLower ? output.LowerCase() : output;
+	return isLower ? output.UpperCase() : output;
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+// const shortHex = prompt("Enter Short Hex.");
+// alert(extendHex(shortHex));
